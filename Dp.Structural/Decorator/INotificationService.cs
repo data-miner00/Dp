66de09217d270f39e7921318ce2338fec87e4056
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-22 23:10:30 +0800

    feat: Added decorator

---
+++ Dp.Structural/Decorator/INotificationService.cs
@@ -0,0 +1,7 @@
+﻿namespace Dp.Structural.Decorator
+{
+    internal interface INotificationService
+    {
+        Task SendEmailAsync(string email);
+    }
+}
---
+++ Dp.Structural/Decorator/MonitoredNotificationService.cs
@@ -0,0 +1,23 @@
+﻿namespace Dp.Structural.Decorator
+{
+    using Sentry;
+
+    internal class MonitoredNotificationService : INotificationService
+    {
+        private readonly INotificationService notificationService;
+        private readonly ISentryClient sentryClient;
+
+        public MonitoredNotificationService(INotificationService notificationService, ISentryClient sentryClient)
+        {
+            this.notificationService = notificationService;
+            this.sentryClient = sentryClient;
+        }
+
+        public async Task SendEmailAsync(string email)
+        {
+            await this.notificationService.SendEmailAsync(email).ConfigureAwait(false);
+
+            this.sentryClient.CaptureEvent(new SentryEvent { Environment = "Dev", Message = "Send email called and success." });
+        }
+    }
+}
