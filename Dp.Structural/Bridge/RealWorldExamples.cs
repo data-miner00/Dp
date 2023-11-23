36d91578cd79f4494d3c131ea87ef84651ad4270
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-23 22:11:14 +0800

    feat: Add bridge

---
+++ Dp.Structural/Bridge/RealWorldExamples.cs
@@ -0,0 +1,16 @@
+﻿namespace Dp.Structural.Bridge
+{
+    internal static class RealWorldExamples
+    {
+        public static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>();
+            ICollection<string> stringCollection = new List<string>();
+            IEnumerable<string> stringEnumerable = new List<string>();
+
+            List<int> intList = new List<int>();
+            ICollection<int> intCollection = new List<int>();
+            IEnumerable<int> intEnumerable = new List<int>();
+        }
+    }
+}
