49b5a3fc50af341729413159d0b1eb0add068fcd
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-23 21:30:16 +0800

    feat: Add composite

---
+++ Dp.Structural/Composite/IComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace Dp.Structural.Composite;
+
+internal interface IComponent
+{
+    IList<IComponent> Children { get; }
+}
+
+internal class RegularComponent : IComponent
+{
+    public string Name = nameof(RegularComponent);
+    public IList<IComponent> Children => null;
+}
+
+internal class OrdinaryComponent : IComponent
+{
+    public string Name = nameof(OrdinaryComponent);
+    public IList<IComponent> Children => null;
+}
+
+internal class CompositeComponent : IComponent
+{
+    public string Name = nameof(CompositeComponent);
+    public IList<IComponent> Children => new List<IComponent>
+    {
+        new RegularComponent(),
+        new OrdinaryComponent(),
+        new CompositeComponent(),
+    };
+}
---
+++ Dp.Structural/Composite/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dp.Structural.Composite
+{
+    internal class LinkedList<T>
+    {
+        public LinkedList() { }
+
+        public LinkedList(T v) => value = v;
+
+        T value;
+        LinkedList<T> next;
+
+        public T Get(int i)
+        {
+            if (next is null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (i == 0)
+            {
+                return value;
+            }
+
+            return next.Get(--i);
+        }
+
+        public void Add(T v)
+        {
+            if (next is null)
+            {
+                value = v;
+                next = new LinkedList<T>();
+            }
+            else
+            {
+                next.Add(v);
+            }
+        }
+    }
+}
