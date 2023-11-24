98502308b4c3d18a981e9c516614ca2088d46f1f
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-24 22:01:24 +0800

    feat: Add flyweight

---
+++ Dp.Structural/Flyweight/UI.cs
@@ -0,0 +1,51 @@
+﻿namespace Dp.Structural.Flyweight
+{
+    /// <summary>
+    /// Extrinsic state should not alter intrinsic state.
+    /// </summary>
+    internal class Icon
+    {
+        public Icon(string path)
+        {
+            // load icon
+        }
+    }
+
+    internal class IconProvider
+    {
+        private static IDictionary<string, Icon> icons;
+
+        public static Icon GetIcon(string type)
+        {
+            if (!icons.ContainsKey(type))
+            {
+                icons[type] = new Icon(type);
+            }
+
+            return icons[type];
+        }    
+    }
+
+    abstract class Button
+    {
+        public Icon Icon { get; set; }
+    }
+
+    class SettingsButton : Button
+    {
+        public SettingsButton()
+        {
+            Icon = IconProvider.GetIcon("settings");
+        }
+    }
+
+    class SolutionWindow
+    {
+        SettingsButton settings = new SettingsButton();
+    }
+
+    class TerminalWindow
+    {
+        SettingsButton settings = new SettingsButton();
+    }
+}
