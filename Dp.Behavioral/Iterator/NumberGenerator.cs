7d9dc42be4b990f2e0d7732a7d48b570dbf793e9
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-25 12:37:00 +0800

    feat: Iterator pattern

---
+++ Dp.Behavioral/Iterator/NumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Dp.Behavioral.Iterator
+{
+    /// <summary>
+    /// Known as cursor or generator. IEnumerable in C#.
+    /// </summary>
+    internal static class NumberGenerator
+    {
+        public static IEnumerable<int> Generate(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
