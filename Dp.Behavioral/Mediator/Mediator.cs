8890caebd75842f4e0b3d81a7c267cee7e2e4549
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-26 07:20:16 +0800

    feat: Mediator

---
+++ Dp.Behavioral/Mediator/Mediator.cs
@@ -0,0 +1,41 @@
+﻿namespace Dp.Behavioral.Mediator;
+
+internal interface IMediator
+{
+    void Interaction();
+}
+
+/// <summary>
+/// Central context of orchestrating interactions.
+/// </summary>
+internal class Mediator : IMediator
+{
+    public Piece piece;
+    public Part part;
+    public Thing thing;
+
+    public void Interaction()
+    {
+        Console.WriteLine("Mediating interaction");
+    }
+}
+
+internal class Piece(IMediator mediator)
+{
+    IMediator mediator = mediator;
+
+    public void Invoke()
+    {
+        mediator.Interaction();
+    }
+}
+
+internal class Part(IMediator mediator)
+{
+    IMediator Mediator = mediator;
+}
+
+internal class Thing(IMediator mediator)
+{
+    IMediator Mediator = mediator;
+}
---
+++ Dp.Behavioral/Mediator/WithMediator.cs
@@ -0,0 +1,37 @@
+﻿namespace Dp.Behavioral.Mediator;
+
+internal class LogicA2
+{
+    DoctorRoom m;
+
+    public void Do()
+    {
+        m.Interaction("b");
+    }
+}
+
+/// <summary>
+/// The context in which the interactions is happening.
+/// </summary>
+public class DoctorRoom : IMediator
+{
+    LogicB2 b;
+
+    public void Interaction() { }
+
+    public void Interaction(string action)
+    {
+        if (action == "b")
+        {
+            b.Do();
+        }
+    }
+}
+
+internal class LogicB2
+{
+    public void Do()
+    {
+        Console.WriteLine("Doing B2 logic");
+    }
+}
