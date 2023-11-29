f7893256086ce5e305dd20c171d2bcc9db0b6122
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-29 20:54:16 +0800

    feat: Memento

---
+++ Dp.Behavioral/Memento/Position.cs
@@ -0,0 +1,28 @@
+﻿namespace Dp.Behavioral.Memento;
+
+internal struct Position
+{
+    private int Cell {  get; set; }
+
+    public class Piece
+    {
+        public Position pos = new();
+
+        public void Move(int n)
+        {
+            this.pos.Cell += n;
+            Console.WriteLine("Moving To " + this.pos.Cell);
+        }
+
+        public Position GetPosition()
+        {
+            return new Position { Cell = this.pos.Cell };
+        }
+
+        public void SetPosition(Position that)
+        {
+            this.pos.Cell = that.Cell;
+            Console.WriteLine("Getting back to " + that.Cell);
+        }
+    }
+}
